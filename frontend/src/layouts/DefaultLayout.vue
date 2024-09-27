20ff41c2922a37d02911c9e87522e2302f8f5fcf
Arthur Brant <arthurcl.br@hotmail.com>
2024-09-27 18:53:56 -0300

    feat: create a default layout component

---
+++ frontend/src/layouts/DefaultLayout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import NavbarMenu from '@/components/NavbarMenu.vue';
+
+</script>
+<template>
+    <div class="flex h-screen">
+        <header class="w-1/4 p-4 border-r-2">
+            <NavbarMenu />
+        </header>
+        <RouterView class="flex-grow p-8" />
+    </div>
+</template>
