656afc5e205c6899c0d5df14088e481341dba71c
Arthur Brant <arthurcl.br@hotmail.com>
2024-09-27 18:59:24 -0300

    feat: create a left side navbar

---
+++ frontend/src/components/NavbarMenu.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from './ui/navigation-menu';
+</script>
+
+<template>
+    <NavigationMenu class="w-full h-full" orientation="vertical">
+        <NavigationMenuList class="flex-col items-start w-full">
+            <NavigationMenuItem class="w-full">
+                <NavigationMenuLink :class="navigationMenuTriggerStyle()" class="mb-4" as-child>
+                    <RouterLink to="/">Home</RouterLink>
+                </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+                <NavigationMenuLink :class="navigationMenuTriggerStyle()" class="mb-4" as-child>
+                    <RouterLink to="/customers">Customers</RouterLink>
+                </NavigationMenuLink>
+            </NavigationMenuItem>
+        </NavigationMenuList>
+    </NavigationMenu>
+</template>
