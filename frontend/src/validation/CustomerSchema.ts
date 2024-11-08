2ded249cfe2b8cdbaaf9ba690c3c7d2ce51072c5
Arthur Brant <arthurcl.br@hotmail.com>
2024-11-08 15:39:43 -0300

    feat:add zod error messages to all properties on CustomerSchema

--- frontend/src/validation/CustomerSchema.ts
+++ frontend/src/validation/CustomerSchema.ts
@@ -2,9 +2,9 @@ import { z } from "zod";
 
 export const CustomerSchema = z.object({
     id: z.string().optional(),
-    firstName: z.string().min(2, "Pelo menos 2 caracteres").max(30),
-    lastName: z.string().min(2, "Pelo menos 2 caracteres").max(150).optional(),
-    phone: z.string().max(11),
+    firstName: z.string().min(2, "Pelo menos 2 caracteres").max(30, "Máximo de 30 caracteres"),
+    lastName: z.string().min(2, "Pelo menos 2 caracteres").max(150, "Máximo de 150 caracteres").optional(),
+    phone: z.string().max(11, "Máximo de 11 números"),
     email: z.string().email("Email inválido"),
 });
 
